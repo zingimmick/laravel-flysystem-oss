716a5c3328dcce598c41fc552bad192551b4235d
Zing <zingimmick@outlook.com>
2022-01-19 23:45:29 +0800

    Initial commit

---
+++ ecs.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Zing\CodingStandard\Set\ECSSetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(ECSSetList::CUSTOM);
+    $containerConfigurator->import(ECSSetList::PHP71_MIGRATION);
+    $containerConfigurator->import(ECSSetList::PHP71_MIGRATION_RISKY);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PARALLEL, true);
+    $parameters->set(
+        Option::PATHS,
+        [__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
+    );
+};
---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zing\Skeleton\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+}
