7626af91eb1f4b20027f9c1da88a89ea1ddc4deb
zingimmick <zingimmick@outlook.com>
2024-10-07 23:31:40 +0800

    More options are supported when creating a client

--- ecs.php
+++ ecs.php
@@ -9,4 +9,9 @@ return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_80, ECSSetList::CUSTOM]);
     $ecsConfig->parallel();
     $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
+    $ecsConfig->skip([
+        \PHP_CodeSniffer\Standards\Generic\Sniffs\Metrics\CyclomaticComplexitySniff::class => [
+            __DIR__ . '/src/OssServiceProvider.php',
+        ],
+    ]);
 };
--- src/OssServiceProvider.php
+++ src/OssServiceProvider.php
@@ -12,6 +12,7 @@ use League\Flysystem\Filesystem;
 use League\Flysystem\PathPrefixing\PathPrefixedAdapter;
 use League\Flysystem\ReadOnly\ReadOnlyFilesystemAdapter;
 use League\Flysystem\Visibility;
+use OSS\Credentials\StaticCredentialsProvider;
 use OSS\OssClient;
 use Zing\Flysystem\Oss\OssAdapter as Adapter;
 use Zing\Flysystem\Oss\PortableVisibilityConverter;
@@ -41,14 +42,56 @@ class OssServiceProvider extends ServiceProvider
                 $options,
                 Arr::only($config, ['url', 'temporary_url', 'endpoint', 'bucket_endpoint'])
             );
-            $ossClient = new OssClient(
-                $config['key'],
-                $config['secret'],
-                $config['endpoint'],
-                $config['bucket_endpoint'] ?? false,
-                $config['token'],
-                $config['proxy'] ?? null
-            );
+            $optionMappings = [
+                'region' => 'region',
+                'endpoint' => 'endpoint',
+                'bucket_endpoint' => 'cname',
+                'signature_version' => 'signatureVersion',
+                'http.proxy' => 'request_proxy',
+            ];
+            foreach ($optionMappings as $standardOption => $clientOption) {
+                if (Arr::has($config, $standardOption)) {
+                    $config[$clientOption] ??= Arr::get($config, $standardOption);
+                }
+            }
+
+            if (class_exists(StaticCredentialsProvider::class)) {
+                if (! isset($config['provider'])) {
+                    $config['provider'] = new StaticCredentialsProvider(
+                        $config['key'],
+                        $config['secret'],
+                        $config['token']
+                    );
+                }
+
+                $ossClient = new OssClient($config);
+            } else {
+                $ossClient = new OssClient(
+                    $config['key'],
+                    $config['secret'],
+                    $config['endpoint'],
+                    $config['bucket_endpoint'] ?? false,
+                    $config['token'],
+                    $config['proxy'] ?? null
+                );
+            }
+
+            if (isset($config['retries'])) {
+                $ossClient->setMaxTries($config['retries']);
+            }
+
+            if (isset($config['http']['timeout'])) {
+                $ossClient->setTimeout($config['http']['timeout']);
+            }
+
+            if (isset($config['http']['connect_timeout'])) {
+                $ossClient->setConnectTimeout($config['http']['connect_timeout']);
+            }
+
+            if (isset($config['schema'])) {
+                $ossClient->setUseSSL($config['schema'] === 'https');
+            }
+
             $ossAdapter = new Adapter(
                 $ossClient,
                 $config['bucket'],
